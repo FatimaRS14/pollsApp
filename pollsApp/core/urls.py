a7a8ca26a7a80253b4cce4cefe1f068f85bad515
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-05-22 14:19:16 -0600

    1.3 back

---
+++ pollsApp/core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<int:q_id>/vote/', views.vote, name='vote'),
+    path('<int:q_id>/results', views.results, name='results'),
+]
--- pollsApp/pollsApp/urls.py
+++ pollsApp/pollsApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 """from django.http import HttpResponse
 
 def hello(request):
@@ -26,6 +26,7 @@ def hello(request):
 """
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     
 ]
